0b23ae39e5883ac4115d185de5d651d6c07eda83
Matthew Amy <meamy@uwaterloo.ca>
2011-08-09 23:30:00 -0400

    Command framework & defaults

--- cmd.ml
+++ cmd.ml
@@ -1,14 +1,49 @@
-module Command = struct
-  let none = 0
-  let left = 1
-  let right = 2
-  let up = 3
-  let down = 4
-  let accept = 5
-  let quit = 6
-  let save = 7
-  let attack = 8
-  let inventory = 9
-  let use = 10
+open Curses;;
 
-  let lookup_cmd key =
+module HT = Hashtbl.Make(struct
+                           type t = int
+                           let equal = (=)
+                           let hash = Hashtbl.hash
+                         end)
+
+type command =
+  | None
+  | Left
+  | Right
+  | Up
+  | Down
+  | Accept
+  | Cancel
+  | Quit
+  | Save
+  | Attack
+  | Inventory
+  | Use
+
+let key_map = HT.create 32
+
+let lookup key =
+  try HT.find key_map key
+  with Not_found -> None
+
+let load_defaults () = 
+  let defaults = [
+    (Key.left, Left);
+    (Key.right, Right);
+    (Key.up, Up);
+    (Key.down, Down);
+    (10, Accept);
+    (27, Cancel);
+    (int_of_char 'q', Quit);
+    (int_of_char 's', Save);
+    (int_of_char 'f', Attack);
+    (int_of_char 'i', Inventory);
+    (int_of_char 'u', Use);
+
+    (* Vikeys *)
+    (int_of_char 'k', Up);
+    (int_of_char 'j', Down);
+    (int_of_char 'h', Left);
+    (int_of_char 'l', Right)]
+  in
+    List.iter (fun (key, cmd) -> HT.add key_map key cmd) defaults
--- main.ml
+++ main.ml
@@ -3,7 +3,7 @@ open Curses
 let width = 30
 let height = 10
 
-module C = struct
+module Colour = struct
   let red = 1
   let green = 2
   let yellow = 3
@@ -32,52 +32,74 @@ let new_game () = ()
 let continue () = ()
 let instructions () = ()
 let credits () = ()
-let exit () = ()
+let exit () = 
+  clear ();
+  endwin ()
 
-let options = [
+let options = [|
   ("New Game", new_game);
   ("Continue", continue);
   ("Instructions", instructions);
   ("Credits", credits);
-  ("Exit", exit)]
-
+  ("Exit", exit)|]
+let nops = Array.length options
 
 let print_title () =
-  attron ((A.color_pair C.green) lor A.bold);
+  attron ((A.color_pair Colour.green) lor A.bold);
 	mvaddstr 3 0 "        88888888  888  888 88888888 doooooo 888  ,dP d88:88o  888\n";
  	addstr       "        88ooooPp  88888888 888  888 d88     888o8P'  888888P  888\n";
  	addstr       "               d8 88P  888 888  888 d88     888 Y8L  88P d8   888\n";
  	addstr       "        8888888P  88P  888 888oo888 d888888 888  `8p 88P  48n 888PPPPP\n";
-  attroff ((A.color_pair C.green) lor A.bold);
+  attroff ((A.color_pair Colour.green) lor A.bold);
   refresh ()
 
 let init () =
+  (* init ncurses *)
   initscr (); (* Start ncurses mode *)
   raw ();     (* Line buffering disabled *)
   keypad (stdscr ()) true; (* Get arrow keys *)
   noecho ();  (* Don't echo chars *)
-  C.init_colours (); (*Initialize colours *)
-  curs_set 0 (* Hide cursor *)
+  Colour.init_colours (); (*Initialize colours *)
+  curs_set 0; (* Hide cursor *)
+  flushinp ();
+
+  (* config *)
+  Cmd.load_defaults ()
 
 let main_menu () =
   let menu_win = newwin 10 60 10 8 in
+  let offs = 2 in
   let print_options () =
-    let rec print_option options i = match options with
-      | [] -> ()
-      | (str, _)::tl -> begin 
-          mvwaddstr menu_win i 2 str;
-          print_option tl (i + 1)
-        end
+    let print_option i (str, _) =
+      ignore (mvwaddstr menu_win (i + offs) 2 str)
     in
-      print_option options 2
+      Array.iteri print_option options
   in
+  let rec get_choice i = 
+    mvwchgat menu_win (i + offs) 1 (width - 3) A.bold 2;
+    wrefresh menu_win;
+    match Cmd.lookup (wgetch menu_win) with
+      | Cmd.Up -> if i > 0 then begin
+          mvwchgat menu_win (i + offs) 1 (width - 3) A.normal 4;
+          get_choice (i - 1)
+        end else get_choice i
+      | Cmd.Down -> if i < (nops-1) then begin
+          mvwchgat menu_win (i + offs) 1 (width - 3) A.normal 4;
+          get_choice (i + 1)
+        end else get_choice i
+      | Cmd.Accept -> 
+          wclear menu_win;
+          delwin menu_win;
+          (snd options.(i)) ()
+      | _ -> get_choice i
+  in
+    keypad menu_win true; (* Get arrow keys *)
     print_title();
-    wattrset menu_win (A.color_pair C.blue);
+    wattrset menu_win (A.color_pair Colour.blue);
     wclear menu_win;
     box menu_win 0 0;
     print_options ();
-    mvwchgat menu_win 2 2 (width - 3) A.bold 2;
-    wrefresh menu_win
+    get_choice 0
 
 let main =
   init ();
