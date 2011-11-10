d2c6e90613f2a973d4d685dc15d9ef008dd1eb10
case <meamy@uwaterloo.ca>
2011-11-10 01:13:04 -0500

    Sorry, this is the add with abstract

---
+++ src/abstract.ml
@@ -0,0 +1,13 @@
+(* Abstract map objects. Define the functions visible to the map *)
+
+open Curses;;
+open Util;;
+
+class virtual actor = object
+end
+
+class virtual obj = object
+  method virtual interact : unit -> unit
+	method virtual get_look : unit -> look
+	method virtual get_properties : unit -> int
+end
